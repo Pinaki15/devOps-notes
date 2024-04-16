3021dc2943a6c31f48c2905048b1e5878d2253e1
Pinaki15 <91341688+Pinaki15@users.noreply.github.com>
2024-04-16 10:07:58 -0500

    Update examples.bash

--- examples.bash
+++ examples.bash
@@ -1,1 +1,32 @@
 
+#! /bin/bash
+#This is my first script
+#Author: Uma
+
+echo "Hello RadheKrishn!!"
+echo ".............................."
+echo " My First Script "
+
+#! /bin/bash
+#Script2-Assigning Variables - prompt for input and store input in a variable
+#Author: Uma
+touch file1 file2
+echo -n "Enter name of the file to be deleted:"
+read fileToBeDeleted
+echo type 'y' to remove 'n' to skip deletion
+rm -i $fileToBeDeleted
+echo "The choses file has been deleted"
+
+
+#! /bin/bash
+#Script2- Assigning Variables - Command Substitution. back tick(`) is used for command substituion
+#Author: Uma
+
+touch file1 file2
+echo -n "Enter name of the file to be deleted:"
+read fileToBeDeleted
+echo type 'y' to remove 'n' to skip deletion
+rm -i $fileToBeDeleted
+echo "The choses file has been deleted"
+
+
