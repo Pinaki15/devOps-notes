88dc0cc91e367e3d0dbf0dda54ef262e9e2f8ae9
Pinaki15 <91341688+Pinaki15@users.noreply.github.com>
2024-04-16 10:11:45 -0500

    Update examples.bash

--- examples.bash
+++ examples.bash
@@ -6,6 +6,7 @@
 echo "Hello RadheKrishn!!"
 echo ".............................."
 echo " My First Script "
+exit
 
 #! /bin/bash
 #Script2-Assigning Variables - prompt for input and store input in a variable
@@ -16,17 +17,18 @@ read fileToBeDeleted
 echo type 'y' to remove 'n' to skip deletion
 rm -i $fileToBeDeleted
 echo "The choses file has been deleted"
-
+exit
 
 #! /bin/bash
 #Script2- Assigning Variables - Command Substitution. back tick(`) is used for command substituion
 #Author: Uma
 
-touch file1 file2
-echo -n "Enter name of the file to be deleted:"
-read fileToBeDeleted
-echo type 'y' to remove 'n' to skip deletion
-rm -i $fileToBeDeleted
-echo "The choses file has been deleted"
+LIST=`ls`
+DATE=`date`
+echo "Today's date is:" $DATE
+echos "The file list:" $LIST
+exit
+
+
 
 
